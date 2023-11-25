fb2eabfa8def25aa51cc588ebb72b6d9363bbed3
Sergiy Poddubka <poseal@gmail.com>
2023-11-25 15:24:43 +0200

    backend add jwt token

--- backend/src/main/java/ua/com/poseal/jwt/backend/controllers/AuthController.java
+++ backend/src/main/java/ua/com/poseal/jwt/backend/controllers/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ua.com.poseal.jwt.backend.config.UserAuthProvider;
 import ua.com.poseal.jwt.backend.dtos.CredentialDto;
 import ua.com.poseal.jwt.backend.dtos.SignUpDto;
 import ua.com.poseal.jwt.backend.dtos.UserDto;
@@ -16,16 +17,19 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final UserAuthProvider userAuthProvider;
 
     @PostMapping("/login")
     public ResponseEntity<UserDto> login(@RequestBody CredentialDto credentialDto) {
         UserDto user = userService.login(credentialDto);
+        user.setToken(userAuthProvider.createToken(user));
         return ResponseEntity.ok(user);
     }
 
     @PostMapping("/register")
     public ResponseEntity<UserDto> register(@RequestBody SignUpDto signUpDto) {
         UserDto user = userService.register(signUpDto);
+        user.setToken(userAuthProvider.createToken(user));
         return ResponseEntity.created(URI.create("/users/" + user.getId())).body(user);
     }
 }
