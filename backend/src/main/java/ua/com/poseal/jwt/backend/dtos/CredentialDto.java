bdeff1a7e324af2602ab1a3cb4d24adbdfc15842
Sergiy Poddubka <poseal@gmail.com>
2023-11-25 16:26:21 +0200

    override methods equals, hashcode, tostring in records

--- backend/src/main/java/ua/com/poseal/jwt/backend/dtos/CredentialDto.java
+++ backend/src/main/java/ua/com/poseal/jwt/backend/dtos/CredentialDto.java
@@ -1,4 +1,31 @@
 package ua.com.poseal.jwt.backend.dtos;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record CredentialDto(String login, char[] password) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CredentialDto that = (CredentialDto) o;
+
+        if (!Objects.equals(login, that.login)) return false;
+        return Arrays.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = login != null ? login.hashCode() : 0;
+        result = 31 * result + Arrays.hashCode(password);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CredentialDto{" +
+                "login='" + login + '\'' +
+                ", password=***}";
+    }
 }
--- backend/src/main/java/ua/com/poseal/jwt/backend/dtos/SignUpDto.java
+++ backend/src/main/java/ua/com/poseal/jwt/backend/dtos/SignUpDto.java
@@ -1,4 +1,37 @@
 package ua.com.poseal.jwt.backend.dtos;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record SignUpDto(String firstName, String lastName, String login, char[] password) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SignUpDto signUpDto = (SignUpDto) o;
+
+        if (!Objects.equals(firstName, signUpDto.firstName)) return false;
+        if (!Objects.equals(lastName, signUpDto.lastName)) return false;
+        if (!Objects.equals(login, signUpDto.login)) return false;
+        return Arrays.equals(password, signUpDto.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(password);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "SignUpDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", login='" + login + '\'' +
+                ", password=***}";
+    }
 }
